5672f75b95844142b2520298dc34d312f6b0205a
Konstantin Volokhin <volokhink@gmail.com>
2016-10-21 16:21:15 +0300

    sql modified

--- sqlscript.sql
+++ sqlscript.sql
@@ -5,12 +5,11 @@ CREATE TABLE tasks(
   name VARCHAR(50) NOT NULL,
   deadline DATE NOT NULL,
   priority VARCHAR(8) NOT NULL,
-  finished TINYINT(1),
-  overdue TINYINT(1)
+  finished TINYINT(1)
                     );
 
-INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('TODO test assigment', '2016-10-19', 'high', 0, 0);
-INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('TODO smth', '2016-10-19', 'medium', 0, 0);
-INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('TODO buy a ticket', '2016-10-19', 'low', 0, 0);
-INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('have a cuo of tea after', '2013-10-19', 'medium', 0, 0);
-INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('get up early', '2013-10-19', 'low', 0, 0);
+INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('TODO test assigment', '2016-10-19', 'high', 0);
+INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('TODO smth', '2016-10-19', 'medium', 0);
+INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('TODO buy a ticket', '2016-10-19', 'low', 0);
+INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('have a cuo of tea after', '2013-10-19', 'medium', 0);
+INSERT INTO tasks(name, deadline, priority, finished, overdue) VALUES ('get up early', '2013-10-19', 'low', 0);
